beecb7b08069b8c868c554e45bebd43ad5631681
Darwin <darwin.a1120@gmail.com>
2026-04-07 17:22:31 -0500

    fix: Se corrige nombre clases

--- src/main/java/com/github/camiloperez77/trackingservice/TrackingService.java
+++ src/main/java/com/github/camiloperez77/trackingservice/TrackingService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class trackingservice{
+public class TrackingService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(trackingservice.class, args);
+		SpringApplication.run(TrackingService.class, args);
 	}
 
 }
